2d1487743e389a9443b9721eaa7a72b206b5716a
Denis Pastory <denispastoty@gmail.com>
2017-09-27 09:54:55 +0900

    Add: airport file

---
+++ Project 2/airports.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Complete the 'extract_airports()' function so that it returns a list of airport
+codes, excluding any combinations like "All".
+
+Refer to the 'options.html' file in the tab above for a stripped down version
+of what is actually on the website. The test() assertions are based on the
+given file.
+"""
+
+from bs4 import BeautifulSoup
+html_page = "options.html"
+
+
+def extract_airports(page):
+    data = []
+    with open(page, "r") as html:
+        # do something here to find the necessary values
+        soup = BeautifulSoup(html, "lxml")
+        select = soup.find("select", {"id": "AirportList"})
+        for opt in select.find_all('option', recursive=False):
+            value = opt["value"]
+            if value[0:3] =="All":
+                continue
+            data.append(opt["value"])
+
+    return data
+
+
+def test():
+    data = extract_airports(html_page)
+    assert len(data) == 15
+    assert "ATL" in data
+    assert "ABR" in data
+
+if __name__ == "__main__":
+    test()
